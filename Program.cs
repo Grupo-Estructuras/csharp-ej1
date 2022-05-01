b15ffb5713e24af0c6893edccf1fe304147716c8
GentleDog <guigapamplona@gmail.com>
2022-05-01 10:14:38 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace csharp_ej1 {
+    class Program {
+        static void Main(string[] args) {
+            HtmlAgilityPack.HtmlWeb web = new HtmlAgilityPack.HtmlWeb();
+            HtmlAgilityPack.HtmlDocument doc = web.Load("");
+            Console.WriteLine("Hello, World!");
+
+        }
+    }
+}
+
