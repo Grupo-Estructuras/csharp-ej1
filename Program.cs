f9ed7628d208494bc904a8c29cf6e52eebbd3505
GentleDog <guigapamplona@gmail.com>
2022-05-01 15:15:09 -0400

    Refactor and add scrapegithub

--- Program.cs
+++ Program.cs
@@ -1,11 +1,17 @@
-﻿namespace csharp_ej1 {
-    class Program {
-        static void Main(string[] args) {
+﻿namespace csharp_ej1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
             List<string> languages = Tiobescraper.scrapeTiobe();
             
-            foreach (var language in languages) {
+            foreach (var language in languages)
+            {
                 Console.WriteLine(language);
             }
+
+            Githubscraper.scrapeGithub(languages);
         }
     }
 }
