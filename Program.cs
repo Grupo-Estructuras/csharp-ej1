8b19e0b3219acdc8c85467e5e6c900e3c7083649
Manuel Rene Pauls Toews <rene.pauls@avantys.de>
2022-05-04 22:08:23 -0400

    Cross OS compatibillity fixes

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System.Diagnostics; 
+﻿using System.Diagnostics;
 
 
 namespace csharp_ej1
@@ -7,32 +7,32 @@ namespace csharp_ej1
     {
         static void Main(string[] args)
         {
-            List<string> languages = Tiobescraper.scrapeTiobe();
-            var orderedLanguages = Githubscraper.scrapeGithub(languages);
+            // List<string> languages = Tiobescraper.scrapeTiobe();
+            // var orderedLanguages = Githubscraper.scrapeGithub(languages);
             var output = ".m" + "p3";
-            var position = 0;
+            // var position = 0;
 
-            foreach (var language in orderedLanguages)
-            {
-                Console.WriteLine($"{++position}- {language.getName()},{language.getRating()},{language.getRepoAmmount()}");
+            // foreach (var language in orderedLanguages)
+            // {
+            //     Console.WriteLine($"{++position}- {language.getName()},{language.getRating()},{language.getRepoAmmount()}");
 
-                if (position == 10) break;
-            }
+            //     if (position == 10) break;
+            // }
 
-            BarChart.generateGraph(orderedLanguages);
+            // BarChart.generateGraph(orderedLanguages);
 
             new Process
             {
-                StartInfo = new ProcessStartInfo($"common\\debug_system_2334{output}")
+                StartInfo = new ProcessStartInfo($"common{System.IO.Path.DirectorySeparatorChar}debug_system_2334{output}")
                 {
                     UseShellExecute = true
                 }
             }.Start();
-            
+
 
             new Process
             {
-                StartInfo = new ProcessStartInfo(@"common\bar_char_1.png")
+                StartInfo = new ProcessStartInfo($"common{System.IO.Path.DirectorySeparatorChar}bar_char_1.png")
                 {
                     UseShellExecute = true
                 }
