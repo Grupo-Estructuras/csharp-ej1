9a811711b7757386c59d72c2a115582c69dc7bac
GentleDog <guigapamplona@gmail.com>
2022-05-04 20:27:34 -0400

    Se crea funcion Barchart

--- graph/BarChart.cs
+++ graph/BarChart.cs
@@ -1,14 +1,32 @@
 
+
 namespace csharp_ej1
 {
     class BarChart
     {
-
-        public static void generateGraph()
+        
+        public static void generateGraph(List<Language> langArr)
         {
-            return;
+            var plt = new ScottPlot.Plot(1200, 800);
+
+            double[] positions = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+            string[] labels = new string[10];
+            double[] values = new double[10];
+
+            for (int i = 0; i < 10; i++)
+            {
+                labels[i] = (langArr[i].getName());
+                values[i] = (langArr[i].getRepoAmmount());
+            }
+
+            plt.AddBar(values, positions);
+            plt.XTicks(positions, labels);
+            plt.SetAxisLimits(yMin: 0);
+
+            plt.SaveFig("bar_graph.png");
         }
-    }        
+    }
 }
+
 
 
