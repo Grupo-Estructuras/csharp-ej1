bde0d80cddb625712cb9787ed9418ce090eb7fb6
GentleDog <guigapamplona@gmail.com>
2022-05-07 16:28:56 -0400

    Refactor y hace generateGraph reutilizable

--- graph/BarChart.cs
+++ graph/BarChart.cs
@@ -4,26 +4,62 @@ namespace csharp_ej1
 {
     class BarChart
     {
-        
-        public static void generateGraph(List<Language> langArr)
+        // Crea un archivo png conteniendo un grafico segun los valores aportados
+        public static void generateGraph(Object objArr, int size, string fileName)
         {
-            var plt = new ScottPlot.Plot(1200, 800);
+            int count = 0;
+            var plt = new ScottPlot.Plot(1900, 1024);
+            double[] positions = new double[size];
+            string[] labels = new string[size];
+            double[] values = new double[size];
 
-            double[] positions = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-            string[] labels = new string[10];
-            double[] values = new double[10];
-
-            for (int i = 0; i < 10; i++)
+            // Vector con la cantidad de valores
+            for (int i = 0; i < size; i++)
             {
-                labels[i] = (langArr[i].getName());
-                values[i] = (langArr[i].getRepoAmmount());
+                positions[i] = (double)i;
             }
 
+            // Tipos de datos soportados para crear el grafico
+            if (objArr is List<Language>)
+            {
+                foreach (var item in (List<Language>)objArr)
+                {
+                    labels[count] = item.getName();
+                    values[count] = item.getRepoAmmount();
+
+                    if (++count >= size) break;
+                }
+            }
+            else if (objArr is IOrderedEnumerable<KeyValuePair<string, int>>)
+            {
+                foreach (var item in (IOrderedEnumerable<KeyValuePair<string, int>>)objArr)
+                {
+                    labels[count] = item.Key;
+                    values[count] = item.Value;
+
+                    if (++count >= size) break;
+                }
+            }
+            else if (objArr is Dictionary<string, int>)
+            {
+                foreach (var item in (Dictionary<string, int>)objArr)
+                {
+                    labels[count] = item.Key;
+                    values[count] = item.Value;
+
+                    if (++count >= size) break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Tipo de dato no soportado");
+            }
+
+            // Configuracion del grafico y guardado
             plt.AddBar(values, positions);
             plt.XTicks(positions, labels);
             plt.SetAxisLimits(yMin: 0);
-
-            plt.SaveFig("bar_graph.png");
+            plt.SaveFig(fileName);
         }
     }
 }
