411986ea09bb7f4e849784bf13336279448b2a81
GentleDog <guigapamplona@gmail.com>
2022-05-07 16:26:26 -0400

    Refactor y funcionalidad parte 2

--- Program.cs
+++ Program.cs
@@ -7,30 +7,40 @@ namespace csharp_ej1
     {
         static void Main(string[] args)
         {
-            // List<string> languages = Tiobescraper.scrapeTiobe();
-            // var orderedLanguages = Githubscraper.scrapeGithub(languages);
-            // var position = 0;
+            var languages = Tiobescraper.scrapeTiobe();
+            var orderedLanguages = Githubscraper.scrapeGithub(languages);
+            var position = 0;
 
-            // foreach (var language in orderedLanguages)
-            // {
-            //     Console.WriteLine($"{++position}- {language.getName()},{language.getRating()},{language.getRepoAmmount()}");
+            foreach (var language in orderedLanguages)
+            {
+                Console.WriteLine($"{++position}- {language.getName()},{language.getRating()},{language.getRepoAmmount()}");
 
-            //     if (position == 10) break;
-            // }
+                if (position == 10) break;
+            }
 
-            // BarChart.generateGraph(orderedLanguages);
+            BarChart.generateGraph(orderedLanguages, 10, "bar_graph.png");
 
-            // new Process
-            // {
-            //     StartInfo = new ProcessStartInfo($"common{System.IO.Path.DirectorySeparatorChar}bar_graph.png")
-            //     {
-            //         UseShellExecute = true
-            //     }
-            // }.Start();
+            new Process
+            {
+                StartInfo = new ProcessStartInfo($"common{System.IO.Path.DirectorySeparatorChar}bar_graph.png")
+                {
+                    UseShellExecute = true
+                }
+            }.Start();
 
-            Githubscraper2.testMethod("Chad");
+            // Parte 2
+            var dic = Githubscraper2.getTopics("chad");
+            var sortedDic = Githubscraper2.sortDictionary(dic);
+            Githubscraper.generateFile(sortedDic, "Resultados2.txt");
+            BarChart.generateGraph(sortedDic, 20, "bar_graph2.png");
 
+            new Process
+            {
+                StartInfo = new ProcessStartInfo($"common{System.IO.Path.DirectorySeparatorChar}bar_graph2.png")
+                {
+                    UseShellExecute = true
+                }
+            }.Start();
         }
     }
 }
-
